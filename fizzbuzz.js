c4500fd2a57194a8682acdcb7590590e997ab386
Steven Mayo <jsmayo91@gmail.com>
2017-10-24 18:06:26 -0400

    Adds: Looping triangle solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,26 @@ When you have that working, modify your program to print "FizzBuzz", for numbers
 
 Excerpt From: Marijn Haverbeke. “Eloquent JavaScript.” iBooks. 
 */
+
 for(var i = 1; i <= 100; i++) {
   if((i % 3) == 0 && (i % 5 == 0)) console.log("FizzBuzz");
   else if((i % 3) == 0) console.log("Fizz");
   else if((i % 5) == 0) console.log("Buzz");
   else console.log(i);
 }
+
+/* 
+“Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+”
+Excerpt From: Marijn Haverbeke. “Eloquent JavaScript.” iBooks. 
+*/
+for(var i = "#"; i.length < 8; i += "#")
+  console.log(i);
